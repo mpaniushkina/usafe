92dcf82b0b65d794d16399ce8dae8f3dce3b134c
142HP <m.paniushkina@softteco.com>
2020-11-26 15:00:13 +0300

    added random keys for companyId and transactionId

--- app/src/main/java/com/covrsecurity/io/model/Fields.java
+++ app/src/main/java/com/covrsecurity/io/model/Fields.java
@@ -1,5 +1,7 @@
 package com.covrsecurity.io.model;
 
+import java.util.UUID;
+
 /**
  * Created by alex on 2.5.16.
  */
@@ -23,6 +25,7 @@ public final class Fields {
     //TODO new keys
     public static final String COVR_SDK_QRCODE_ID = "d23857eb-c1c1-4b5b-bcc1-d2ec58d131f5";
     public static final String COVR_SDK_QRCODE_SECRET = "2ccd4da0-795b-4e07-b6b7-78750dd91d5a";
-    public static final String GET_QRCODE_COMPANY_USER_ID = "7A4F612C-3095-4E6D-9851-EB9E8778DE48";
-    public static final String GET_QRCODE_TRANSACTION_ID = "7A4F612C-3095-4E6D-9851-EB9E8778DE48";
+    public static final String GET_QRCODE_COMPANY_USER_ID = UUID.randomUUID().toString();
+    public static final String GET_QRCODE_TRANSACTION_ID = UUID.randomUUID().toString();
+
 }
